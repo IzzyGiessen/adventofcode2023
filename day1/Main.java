2f14f09a5d305259f9a3ed32c06d52aed893101b
Izzy van der Giessen <i.vandergiessen@student.tudelft.nl>
2024-09-09 17:52:37 +0200

    remove folder

--- day1/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        Scanner s = new Scanner(new FileReader("input/sample"))
-        System.out.println(s.nextLine());
-    }
-}
